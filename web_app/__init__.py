274d4a8b245015a1577038b6be679522b42fbee7
ksimon17 <78188196+ksimon17@users.noreply.github.com>
2022-04-26 23:04:13 -0400

    working on firebase implementation and html formatting

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -7,6 +7,7 @@ from flask import Flask
 from authlib.integrations.flask_client import OAuth
 
 from app import APP_ENV, APP_VERSION
+from app.firebase_service import FirebaseService
 
 from web_app.routes.home_routes import home_routes
 from web_app.routes.login_routes import login_routes
@@ -21,6 +22,9 @@ load_dotenv()
 # for google oauth login:
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID") # need to setup
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET") # need to setup
+
+# for google analytics (universal analytics):
+GA_TRACKER_ID = os.getenv("GA_TRACKER_ID", default="G-OOPS")
 
 
 SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
@@ -38,6 +42,11 @@ def create_app():
     # app.config["NAV_ICON_CLASS"] = NAV_ICON_CLASS
     # app.config["NAV_COLOR_CLASS"] = NAV_COLOR_CLASS
 
+    # for client-side google analytics:
+    app.config["GA_TRACKER_ID"] = GA_TRACKER_ID
+
+    
+
     os.environ["TZ"] = "UTC"
 
     oauth = OAuth(app)
@@ -52,6 +61,14 @@ def create_app():
     app.config["OAUTH"] = oauth
 
 
+    #
+    # SERVICES
+    #
+  #  app.config["FIREBASE_SERVICE"] = FirebaseService()
+
+
+
+    # ROUTES
     app.register_blueprint(home_routes)
     app.register_blueprint(login_routes)
     app.register_blueprint(recipe_routes)
