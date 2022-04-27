307a6eeb0e18a233284ceacab5077e0cb263a641
ksimon17 <78188196+ksimon17@users.noreply.github.com>
2022-04-27 00:08:33 -0400

    Firebase Setup and HTML Formatting (#7)
    * working on firebase implementation and html formatting
    
    * Delete google-credentials.json

---
+++ web_app/routes/user_routes.py
@@ -0,0 +1,60 @@
+
+from flask import Blueprint, render_template, flash, redirect, current_app, url_for, session, request #, jsonify
+
+from web_app.routes.wrappers import authenticated_route
+
+user_routes = Blueprint("user_routes", __name__)
+
+#
+# USER ORDERS
+#
+
+@user_routes.route("/user/orders")
+@authenticated_route
+def orders():
+    print("USER ORDERS...")
+    current_user = session.get("current_user")
+    service = current_app.config["FIREBASE_SERVICE"]
+    orders = service.fetch_user_orders(current_user["email"])
+    return render_template("user_orders.html", orders=orders)
+
+
+@user_routes.route("/user/orders/create", methods=["POST"])
+@authenticated_route
+def create_order():
+    print("CREATE USER ORDER...")
+
+    form_data = dict(request.form)
+    print("FORM DATA:", form_data)
+    print(form_data["recipe_name"])
+    print(form_data["video_url"])
+    product_info = {
+        "name": form_data["recipe_name"],
+        "video_url": form_data["video_url"]
+    }
+
+    current_user = session.get("current_user")
+
+    service = current_app.config["FIREBASE_SERVICE"]
+
+    try:
+        service.create_order(user_email=current_user["email"], product_info=product_info)
+        flash(f"Recipe Added!", "success")
+        return redirect("/user/orders")
+    except Exception as err:
+        print(err)
+        flash(f"Oops, something went wrong: {err}", "warning")
+        return redirect("/products")
+
+
+#
+# USER PROFILE
+#
+
+@user_routes.route("/user/profile")
+@authenticated_route
+def profile():
+    print("USER PROFILE...")
+    current_user = session.get("current_user")
+    #user = fetch_user(email=current_user["email"])
+    return render_template("user_profile.html", user=current_user) # user=user
