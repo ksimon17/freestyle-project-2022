51d4bbcb4eec7442e77bdd2a48c38e454b9c1983
ksimon17 <78188196+ksimon17@users.noreply.github.com>
2022-05-03 15:35:37 -0400

    Refactored Code based on Code Climate (#16)

--- web_app/routes/recipe_routes.py
+++ web_app/routes/recipe_routes.py
@@ -20,16 +20,15 @@ def index():
 def recipe_list():
     print("Recipe List...")
 
+    #Read in form_data and convert to dictionary
     form_data = dict(request.form)
-    #print(form_data)
 
+    #Read in variables from form data
     method = form_data["method"]
     selection = form_data["selection"]
     email = form_data["email"]
-    print(method)
-    print(selection)
-    print(email)
 
+    #Return a list of dictionaries of recipes based on the user's method choice
     if method == "By Category":
         recipes = display_category(selection)
     elif method == "By Nationality/Country of Origin":
@@ -38,20 +37,21 @@ def recipe_list():
         recipes = display_ingredient(selection)
     elif method == "Random Selection":
         recipes = display_random()
-    #pprint(recipes)
 
-    #creating the HTML template and sending email
+    #creating the HTML template and sending personalized email to user
     if email:
+
+        #create subject
         subject = "Custom Recipe List - Recipe Generator App Search"
         
-        
+        #create body
         html = ""
         html += f"<h>Recipe Generator Search Results</h>"
         html += "<br>"
         html += f"<p>Here is you individual recipe list based on your search: .</p>"
         html += "<br>"
-        # html += "<ul>"
         
+        #iterate over each recipe in the search and display information to the user in the body of the email
         for recipe in recipes:
             html += f"<h> {recipe['name']} </h>"
             html += "<br>"
@@ -68,16 +68,19 @@ def recipe_list():
             html += "</ul>"
             html += "<br>"
             html += "<hr>"
-        # html += "</ul>"
 
+        # send the appropriate email to the user's email address
         send_email(subject, html, email)
 
     
+    #Handle Errors where the user does not implement correct information in the search enginge 
     try:
         if recipes:
+            # Correctly take the user to their list of recipes based on their search results
             flash(f"Recipe Search Successful!", "success")
             return render_template("recipes_list.html", recipes=recipes)
         else:
+            # The user did not correctly input selections in Search Engine - gracefully handles such error
             flash(f"Oops, something went wrong. Please correctly enter your inputs based on your instructions. You can refer to the Help Page to see the valid inputs. If an ingredient or recipe is in the help page but is still is procuding errors, please contact the Admin at 'kms475@georgetown.edu':", "warning")
             return redirect("/home")
 
