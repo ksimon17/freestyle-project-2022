3a28800654befff92d162987c67d1b5f5b526e2b
ksimon17 <78188196+ksimon17@users.noreply.github.com>
2022-05-04 11:22:22 -0400

    code for skipping tests in Github Actions

--- tests/webapp_test.py
+++ tests/webapp_test.py
@@ -3,16 +3,20 @@ import pytest
 from web_app import create_app
 
 @pytest.fixture(scope="module")
+
+@pytest.mark.skipif(os.getenv("CI")=="true", reason="will not pass because local google credential files is not on Github/any server")
 def test_client():
     app = create_app()
     app.config.update({"TESTING": True})
     return app.test_client()
 
+@pytest.mark.skipif(os.getenv("CI")=="true", reason="will not pass because local google credential files is not on Github/any server")
 def test_home(test_client):
     response = test_client.get("/")
     assert response.status_code == 200
     assert b"Home" in response.data
 
+@pytest.mark.skipif(os.getenv("CI")=="true", reason="will not pass because local google credential files is not on Github/any server")
 def test_about(test_client):
     response = test_client.get("/about")
     assert response.status_code == 200
@@ -33,11 +37,13 @@ def test_about(test_client):
 #     assert response.status_code == 200
 #     assert b"Custom Recipe List" in response.data
 
+@pytest.mark.skipif(os.getenv("CI")=="true", reason="will not pass because local google credential files is not on Github/any server")
 def test_help(test_client):
     response = test_client.get("/help")
     assert response.status_code == 200
     assert b"List of All Possible Inputs For Recipe Generator" in response.data
 
+@pytest.mark.skipif(os.getenv("CI")=="true", reason="will not pass because local google credential files is not on Github/any server")
 def test_login(test_client):
     response = test_client.get("/login")
     assert response.status_code == 200
